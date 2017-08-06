402069e9d693184f2687369453fc4c31b848483d
chuong_phan <chuong.phan@secomm.vn>
2017-08-06 23:13:03 +0700

    init config

--- application/config/config.php
+++ application/config/config.php
@@ -232,7 +232,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '1989-school-project';
 
 /*
 |--------------------------------------------------------------------------
@@ -301,7 +301,7 @@ $config['global_xss_filtering'] = FALSE;
 | 'csrf_cookie_name' = The cookie name
 | 'csrf_expire' = The number in seconds the token should expire.
 */
-$config['csrf_protection'] = FALSE;
+$config['csrf_protection'] = TRUE;
 $config['csrf_token_name'] = 'csrf_test_name';
 $config['csrf_cookie_name'] = 'csrf_cookie_name';
 $config['csrf_expire'] = 7200;
